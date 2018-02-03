275a6323277f9203e322c629ad44f950560357d8
willejka <32966987+willejka@users.noreply.github.com>
2018-02-03 12:33:42 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,9 +2,9 @@ var i = 0,
     j = 0;
     
 function drawTree () {
-  for (var i=0; i<6; i++) { 
+  for (var i=0; i<5; i++) { 
         var star = ' ';
-          for (var j=0; j<i; j++) {
+          for (var j=0; j<=i; j++) {
         star += '* ';        
       }
     console.log(star);
